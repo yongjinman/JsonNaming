585dbd88434d51c64b2ac3936d3bdd1377c2c572
yjmoon <yjmoon@fletamuto.com>
2015-09-07 18:10:50 +0900

    update maven

---
+++ src/main/java/kr/kro/yongjinman/App.java
@@ -0,0 +1,16 @@
+
+package kr.kro.yongjinman;
+
+import kr.kro.yongjinman.json.JsonNamingObject;
+
+/**
+ * Hello world!
+ */
+public class App
+{
+
+    public static void main( String[] args ) {
+        JsonNamingObject pJsonObject = new JsonNamingObject();
+        System.out.println( "Hello World!" );
+    }
+}
